fe8b5ace36c4bc872beb1831744ce9b9b2abc6e6
srikanthkssiddu <94665621+srikanthkssiddu@users.noreply.github.com>
2022-04-04 18:33:00 +0530

    Add files via upload

---
+++ april4rth/arrays.js
@@ -0,0 +1,4 @@
+// let array = [1,3,5,7,9];
+// s=array.join("");
+// console.log(s);
+
---
+++ april4rth/objects and strings.js
@@ -0,0 +1,9 @@
+// var objects = {
+//     name:"srikanth",sclass:"topentile",rollno:'28'
+// }
+// var s=Object.keys(objects).length;
+// console.log("length of object is",s);
+
+let string='srikanth';
+console.log(string.charAt('0').toUpperCase()+string.slice(1));
+
