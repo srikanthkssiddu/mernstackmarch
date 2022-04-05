f932ce49aabc459bb25b80eeca21a06971e1e569
srikanthkssiddu <94665621+srikanthkssiddu@users.noreply.github.com>
2022-04-05 18:23:41 +0530

    Add files via upload

--- april4rth/objects and strings.js
+++ april4rth/objects and strings.js
@@ -1,9 +1,77 @@
-// var objects = {
+// 
+// 1
+//  var objects = {
 //     name:"srikanth",sclass:"topentile",rollno:'28'
 // }
 // var s=Object.keys(objects).length;
 // console.log("length of object is",s);
 
-let string='srikanth';
-console.log(string.charAt('0').toUpperCase()+string.slice(1));
+// 2
+// let string='srikanth';
+// console.log(string.charAt('0').toUpperCase()+string.slice(1));
+// 3
+// let firstname ="srikanth";
+// let lastname="ks";
+// let named = firstname.concat(lastname);
+// console.log(named);
+// 4
+// let thename="sikanth";
+// let missingletter ='r';
+// console.log(thename.splice(0,1)+missingletter+thename.slice(1));
+
+// 5
+// let string="srikanth k s";
+// let n=2;
+// let arr = [];
+// let j=0;
+// for(i=0;i<string.length;i=i+n){
+//     arr[j]=string.slice(i,i+n);
+//     j++
+// }
+// console.log(arr);
+//
+// let string = "srikanth is going to home";
+// let sp = string.split(" ");
+// console.log(sp.length);
+// 6
+// function occurance(string){
+//     let count = 0;
+//     let sp = string.split(" ");
+//     for(i=0;i<string.length;i++);{
+
+    
+//     if (sp[i]='the')
+//     {
+//         count++
+//     }
+// }
+// console.log(count);
+
+// }
+// console.log(occurance('the cat is the mother of 2 babies'));
+// 7
+// let string = "srikanth";
+// let rep = string.repeat(3);
+// console.log(rep);
+
+
+// 8
+// function specified(string,char){
+//     index = string.indexOf(char);
+//     new_string=string.splice(index);
+//     return new_string;
+// }
+// console.log(specified("srikanth","i"));
+
+// function search(string, char){
+//     let index = string.indexOf(char);
+//     // let array1 = string.slice(0,index);
+//     // let array2 = string.slice(index)
+//     console.log(string.slice(0,index) + string.slice(index + char.length));
+// }
+// char = "are";
+// string = "we are going to college";
+// search(string,char);
+
+
 
